d8b432602469940dfda62638476bcf484af30749
Arslan Ahmad <ars.ahm6@gmail.com>
2019-02-21 13:17:38 +0500

    GMail basic reading examples added

---
+++ GMailClient/Practice/GMailHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace Practice
+{
+    static class GMailHelper
+    {
+        public static GmailService GetGMailService(string[] scopes, string applicationName)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream(@"Data\credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName,
+            });
+
+            return service;
+        }
+
+        public static List<string> GetLabels(GmailService service)
+        {
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            IList<Label> labels = request.Execute().Labels;
+            return labels.Select(l => l.Name).ToList();
+        }
+
+        public static List<Draft> GetDrafts(GmailService service)
+        {
+            UsersResource.DraftsResource.ListRequest request = service.Users.Drafts.List("me");
+            var drafts = request.Execute();
+            return drafts.Drafts.ToList();
+        }
+
+        public static Draft GetDraft(GmailService service, string draftId)
+        {
+            UsersResource.DraftsResource.GetRequest request = service.Users.Drafts.Get("me", draftId);
+            var draft = request.Execute();
+            return draft;
+        }
+    }
+}
--- GMailClient/Practice/Program.cs
+++ GMailClient/Practice/Program.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace Practice
 {
     class Program
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] _scopes = { GmailService.Scope.GmailReadonly };
+        static string _applicationName = "Gmail API .NET Quickstart";
+
         static void Main(string[] args)
         {
+            var service = GMailHelper.GetGMailService(_scopes, _applicationName);
+
+            var labels = GMailHelper.GetLabels(service);
+            PrintLabels(labels);
+
+            var drafts = GMailHelper.GetDrafts(service);
+            var draft = GMailHelper.GetDraft(service, "r-3167230408123907724");
+            
+        }
+
+        static void PrintDrafts(List<Draft> drafts)
+        {
+
+        }
+
+        static void PrintLabels(List<string> labels)
+        {
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
         }
     }
 }
